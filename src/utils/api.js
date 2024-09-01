130fb0ae7eda2af201534d6489994fce44c52277
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-31 22:08:03 -0700

    12. (✨2024 Latest Update) Build a Netflix Clone Lecture 6~7

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,18 +3,20 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const api = axios.create({
-    baseURL: 'http://api.themoviedb.org/3',
+    baseURL: 'https://api.themoviedb.org/3', // HTTPS로 수정
     headers:{
         Accept: 'application/json',
-        Authorization: `Bearer ${API_KEY}`
+    },
+    params: { // URL 파라미터로 API 키를 전달
+        api_key: API_KEY,
     }
 });
 
 axios.interceptors.response.use(
-    function (response){
+    function (response) {
         return response;
     },
-    function (error){
+    function (error) {
         return Promise.reject(error);
     }
 );
