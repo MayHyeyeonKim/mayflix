09e07946adaeedb99794ed3cb9cf9374ad5ec0a5
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-31 22:14:10 -0700

    tried to change baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,23 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY; // 환경 변수에서 API 키 가져오기
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3', // HTTPS로 수정
-    headers:{
-        Accept: 'application/json',
-    },
-    params: { // URL 파라미터로 API 키를 전달
+    baseURL: '/', // 프록시 설정에 의해 올바른 경로로 라우팅
+    params: {
         api_key: API_KEY,
+    },
+    headers: {
+        Accept: 'application/json',
     }
 });
 
-axios.interceptors.response.use(
+api.interceptors.response.use(
     function (response) {
         return response;
     },
     function (error) {
+        console.error('API 호출 오류:', error.response ? error.response.data : error.message);
         return Promise.reject(error);
     }
 );
