10466fdbec8e22c3e3c172d2f33d34c37963c9f1
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-31 22:00:11 -0700

    banner design

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,11 +1,41 @@
 import {usePopularMoviewsQuery} from "../../../../hooks/usePopularMovies"
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
+import './Banner.style.css'
+import { useEffect, useState } from "react";
 
 const Banner = () => {
-    const { data } = usePopularMoviewsQuery();
+    const { data, isLoading, isError, error } = usePopularMoviewsQuery();
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
     console.log("data: ", data);
+
+    useEffect(()=>{
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    },[])
+
+    if(isLoading){
+        <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    }
+    if(isError){
+        <Alert variant={'danger'}> {error.message} </Alert>
+    }
+
+    const bannerImage = isMobile
+    ? `url(https://media.themoviedb.org/t/p/w300${data?.results[0].poster_path})`
+    : `url(https://media.themoviedb.org/t/p/w533_and_h300_bestv2${data?.results[0].poster_path})`;
+
     return (
-        <div>
-            Banner
+        <div style={{backgroundImage: bannerImage }} className="banner">
+            <div className="text-white banner-text-area">
+                <h1 className='title'>{data?.results[0].title}</h1>
+                <p className='overview'>{data?.results[0].overview}</p>
+            </div>
         </div>
     )
 }
