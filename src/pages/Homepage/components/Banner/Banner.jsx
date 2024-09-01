a138f960af996f0a07bd46a21173fc8ddfb72148
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-31 22:32:09 -0700

    fixed: No movie data available3 열받아

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -6,15 +6,6 @@ import { useEffect, useState } from "react";
 
 const Banner = () => {
     const { data, isLoading, isError, error } = usePopularMoviewsQuery();
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth < 768);
-        };
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
 
     if (isLoading) {
         return (
@@ -32,19 +23,11 @@ const Banner = () => {
         return <Alert variant={'warning'}>No movie data available</Alert>;
     }
 
-    // 데이터가 없는 경우 기본 이미지 설정 또는 `poster_path` 존재 확인
-    const posterPath = data.results[0].poster_path ? data.results[0].poster_path : "";
-    const bannerImage = posterPath
-        ? (isMobile
-            ? `url(https://media.themoviedb.org/t/p/w300${posterPath})`
-            : `url(https://media.themoviedb.org/t/p/w533_and_h300_bestv2${posterPath})`)
-        : 'url(/path/to/default-image.jpg)'; // 기본 이미지 URL
-
     return (
-        <div style={{ backgroundImage: bannerImage }} className="banner">
+        <div style={{ backgroundImage: `url(https://media.themoviedb.org/t/p/w533_and_h300_bestv2${data?.results[0].poster_path}` }} className="banner">
             <div className="text-white banner-text-area">
-                <h1 className='title'>{data.results[0].title}</h1>
-                <p className='overview'>{data.results[0].overview}</p>
+                <h1 className='title'>{data?.results[0].title}</h1>
+                <p className='overview'>{data?.results[0].overview}</p>
             </div>
         </div>
     );
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,20 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_API_KEY; // 환경 변수에서 API 키 가져오기
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const api = axios.create({
-    baseURL: '/', // 프록시를 통해 기본 URL 설정
-    params: {
-        api_key: API_KEY, // API 키를 URL 파라미터로 추가
-    },
-    headers: {
+    baseURL: 'http://api.themoviedb.org/3',
+    headers:{
         Accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
     }
 });
 
-// 응답 인터셉터 설정
-api.interceptors.response.use(
-    function (response) {
+axios.interceptors.response.use(
+    function (response){
         return response;
     },
-    function (error) {
-        console.error('API 호출 오류:', error.response ? error.response.data : error.message);
+    function (error){
         return Promise.reject(error);
     }
 );
