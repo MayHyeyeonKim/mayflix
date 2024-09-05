03eda39a58123ab29759d1ddc3618e1af8d312aa
MayHyeyeonKim <hkim7963@gmail.com>
2024-09-04 20:31:12 -0700

    12. (✨2024 Latest Update) Build a Netflix Clone Lecture 13~14

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -4,53 +4,44 @@ import { Container, Row, Col } from "react-bootstrap";
 import ReactPaginate from "react-paginate";
 import "./MoviePage.style.css";
 
-//경로 2가지
-// nav바에서 클릭해서 온 경우 -> popularMovie
-// keyword를 입력해서 온 경우 -> keyword와 관련된 영화들을 보여줌
-
-//페이지네이션 설치
-//page state 만들기
-//페이지네이션 클릭할 때마다 페이지 바꿔주기
-//page값 바뀔 때마다 useSearchMovie에 page까지 넣어서 fetch
-
 import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
 import MovieCard from "../../common/MovieCard/MovieCard";
 
 const MoviePage = () => {
   const [query, setQuery] = useSearchParams();
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(1);
   const keyword = query.get("q");
-  const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword, page });
-  const handlePageClick = ({selected}) => {
-    setPage(selected+1)
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+
+  const handlePageClick = ({ selected }) => {
+    setPage(selected + 1);
   };
+
   console.log("여기는 MoviePage의 data", data);
+
   return (
     <Container>
-      <Row>
-        <Col lg={4} xs={12}>
-          {/* 필터 */}
-        </Col>
+      {/* <Row> */}
         <Col lg={8} xs={12}>
-        <Row>
+          {/* Row에 gutter를 설정하여 카드 간격 조정 */}
+          <Row className="g-4"> 
             {isLoading ? (
               <p>검색 하기</p> // 로딩 중일 때 표시할 메시지
             ) : data && data.results.length === 0 ? (
               <p>검색 결과가 없습니다.</p> // 검색 결과가 없을 때 표시할 메시지
             ) : (
               data?.results.map((movie, index) => (
-                <Col key={index} lg={6} xs={12}>
+                <Col key={index} lg={4} xs={12} className="mx-14 mb-4">
+
+                    {/* lg={2}로 설정하여 한 줄에 6개씩 배치 */}
                   <MovieCard movie={movie} />
+
                 </Col>
               ))
             )}
-          </Row>
-          <Row>
-            {data?.results.map((movie, index) => (
-              <Col key={index} lg={6} xs={12}>
-                <MovieCard movie={movie} />
-              </Col>
-            ))}
           </Row>
           <ReactPaginate
             nextLabel="next >"
@@ -74,8 +65,9 @@ const MoviePage = () => {
             forcePage={page - 1}
           />
         </Col>
-      </Row>
+      {/* </Row> */}
     </Container>
   );
 };
+
 export default MoviePage;
